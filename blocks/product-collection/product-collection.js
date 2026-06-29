74f839fa846a00f7fc395e2e5de40314054b128d
Ritesh Singh <ritesh.singh@brainvire.com>
2026-06-29 16:19:53 +0530

    Product collection fix

--- blocks/product-collection/product-collection.js
+++ blocks/product-collection/product-collection.js
@@ -1,7 +1,3 @@
 export default function decorate(block) {
-  const cards = [...block.children];
-
-  cards.forEach((card) => {
-    card.classList.add('card-item');
-  });
+  block.textContent = 'Product Collection Loaded';
 }
