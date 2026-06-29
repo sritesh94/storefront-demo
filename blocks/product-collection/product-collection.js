7d8cbaa09d2daa9e05075ccf9282d14ce3813f12
Ritesh Singh <ritesh.singh@brainvire.com>
2026-06-29 16:04:23 +0530

    Product collection code

--- blocks/product-collection/product-collection.js
+++ blocks/product-collection/product-collection.js
@@ -2,17 +2,5 @@ import '../../scripts/initializers/search.js';
 import { search } from '@dropins/storefront-product-discovery/api.js';
 
 export default async function decorate(block) {
-  const result = await search({
-    phrase: '',
-    currentPage: 1,
-    pageSize: 1,
-    filter: [
-      {
-        attribute: 'visibility',
-        in: ['Catalog, Search', 'Search'],
-      },
-    ],
-  });
-
-  block.innerHTML = `<pre>${JSON.stringify(result, null, 2)}</pre>`;
+  block.textContent = 'Product Collection Loaded';
 }
